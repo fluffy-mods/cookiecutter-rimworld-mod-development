d23e3f5c54483d44d573413a1cc6b36ff2e74352
Fluffy <fluffy.l2032@gmail.com>
2020-04-20 20:37:54 +0200

    fix some tweaks

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,6 @@
 import os
 
 #About is always made
-os.makedirs("{{cookiecutter.mod_name}}/Defs", exist_ok=True)
-os.makedirs("{{cookiecutter.mod_name}}/Patches", exist_ok=True)
-os.makedirs("{{cookiecutter.mod_name}}/Languages", exist_ok=True)
-os.makedirs("{{cookiecutter.mod_name}}/Sounds", exist_ok=True)
-os.makedirs("{{cookiecutter.mod_name}}/Textures", exist_ok=True)
+os.makedirs("/Defs", exist_ok=True)
+os.makedirs("/Languages", exist_ok=True)
+os.makedirs("/Textures", exist_ok=True)
