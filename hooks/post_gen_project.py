78aad0aac42aee06f29eb69b6045fa14fee615fe
Fluffy <fluffy.l2032@gmail.com>
2020-04-20 21:52:49 +0200

    up harmony version, add RW dlls, tweak post-gen hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,7 +6,17 @@ os.makedirs("/Languages", exist_ok=True)
 os.makedirs("/Textures", exist_ok=True)
 
 {% if(cookiecutter.version_control != 'n') %}
+
+# stuff specifically for my release script(s);
+# create repository on fluffy-mods
 os.system("rw-release init \"{{cookiecutter.mod_name.replace(' ', '')}}\" \"{{cookiecutter.short_desc}}\"")
+
+# create release branch for mod version
 os.system("rw-release create {{cookiecutter.mod_ver}}")
+
+# update/create generated files
+os.system("mod update")
+
+# set upstream and use branch as default
 os.system("rw-release use {{cookiecutter.mod_ver}}")
 {% endif %}
